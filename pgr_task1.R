cb45df3bc362693b5663671008effb03c58903a2
emalorkova <62432570+emalorkova@users.noreply.github.com>
2022-10-13 10:29:02 +0200

    RecursiveSelectionSort oprava

--- pgr_task1.R
+++ pgr_task1.R
@@ -1,3 +1,5 @@
+library(seqinr)
+
 IndexOfMin <- function(array, first, last) {
   index <- first
   for (k in (first + 1):last) {
@@ -25,13 +27,11 @@ RecursiveSelectionSort <- function(array, first, last) {
   if (first < last) {
     index <-IndexOfMin(array, first, last)
     #Swap array(first) with array(index)
-    temp <- array[first]
-    array[first] <- array[index]
-    array[index] <- temp
-    array <- RecursiveSelectionSort(array, first + 1, last)
+    swap(array[first], array[index])
+    array <- RecursiveSelectionSort(array, (first + 1), last)
   }
 return(array)
 }
 
 
-RecursiveSelectionSort(c(5,6,7,9,4,3,2,1, 10, 17), 1, 8)
+RecursiveSelectionSort(c(5,6,7,9,4,3,2,1, 10, 17), 3, 8)
